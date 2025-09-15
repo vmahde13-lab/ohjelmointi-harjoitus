4cc27da2f78bc25ce43e170c081626535f7dd303
Valtteri Markus Ahde <vmahde13@gmail.com>
2025-09-15 14:55:59 +0300

    Lisäsin projektin
    testi

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App
+ {
+    public static void main(String[] args) throws Exception {
+    
+    Scanner in = new Scanner(System.in);
+    int counter = 0;
+
+    while(true){
+        System.out.println("Guess my name (type stop to exit)");
+        String name1 = in.next();
+
+    if(name1.equalsIgnoreCase("stop")){
+        System.out.println("You guessed " + counter + " times.");
+    break;
+    }
+
+    counter++;
+
+    if(name1.equalsIgnoreCase("Emma")){
+        System.out.println("Congratulations!");
+        System.out.println("You guessed " + counter + " times.");
+    break;
+    }
+}
+}
+}
