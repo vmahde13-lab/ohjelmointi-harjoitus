ed049d60e583569ea831db6b2692069e63d7f032
Valtteri Markus Ahde <vmahde13@gmail.com>
2025-09-15 14:59:21 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App
  {
     public static void main(String[] args) throws Exception {
     
+    System.out.println("Github juttu :D !");
+
     Scanner in = new Scanner(System.in);
     int counter = 0;
 
